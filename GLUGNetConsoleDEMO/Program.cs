187d06b7b1f2c563c212aa5f6356faa88f0bf785
GitHubTuna <31992693+GitHubTuna@users.noreply.github.com>
2017-09-16 20:06:49 -0400

    Added Link to SQL Server GitHub Links

--- GLUGNetConsoleDEMO/Program.cs
+++ GLUGNetConsoleDEMO/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace GLUGNetConsoleDEMO
 {
+    //SQL Server Sample Code
+    //https://github.com/Microsoft/sql-server-samples/tree/master/samples
+
     //Created
     //www.FortunaJacob.VisualStudio accessing Git inside of this "MyFirstProject"...
     //https://github.com/GitHubTuna/GLUGNetDEMOS/blob/master/GLUGNetConsoleDEMO/Program.cs
