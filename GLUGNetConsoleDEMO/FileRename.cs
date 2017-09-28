5b5db7d707ba0e55e42e86942b8533d75213ccd0
GitHubTuna <31992693+GitHubTuna@users.noreply.github.com>
2017-09-27 21:16:14 -0400

    Commit Changes to Branch to Create Files

--- GLUGNetConsoleDEMO/FileRename.cs
+++ GLUGNetConsoleDEMO/FileRename.cs
@@ -13,25 +13,33 @@ namespace GLUGNetConsoleDEMO
         public int TotalRecords = 0;
 
         public void RenameFiles()
-        //Ready for TM Presentation: Files are currently named "LESSON"
+
         {
-            // @ makes "\" read as a directory
+            //Loop with LINQ as possible enhancement
+            //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/how-to-query-for-duplicate-files-in-a-directory-tree-linq
+
             string FileLocation = @"C:\Fortuna";
 
-            if (Directory.Exists(FileLocation))
+            //Initialize Files 
+            for (int i = 1; i < 4; i++)
             {
-                //foreach (var folder in Directory.GetDirectories(FileLocation))
-                //{
+                if (!File.Exists(FileLocation + @"\Text" + i + ".txt") && !File.Exists(FileLocation + @"\Jacob" + i + ".txt"))
+                {
+                    File.Create(FileLocation + @"\Text" + i + ".txt").Close();
+                    //Close is needed or the file will be locked when you try to move it
+                }
+            }
+
+            if (Directory.Exists(FileLocation))
+            {   
                 foreach (var file in Directory.GetFiles(FileLocation))
                 {
-                    //if (file.EndsWith("_4.jpg"))
-                    //{
-                    
                     string filename = new FileInfo(file).Name;
 
                     string testString;
                     string replaceString;
-                    
+
+                    //Initialize Files to be Renamed based upon what is found:
                     testString = "Jacob";
                     if (filename.IndexOf(testString, 0, StringComparison.CurrentCultureIgnoreCase) != -1)
                     {
@@ -42,7 +50,6 @@ namespace GLUGNetConsoleDEMO
                         testString = "Text";
                         replaceString = "Jacob";
                     }
-                        
 
                     if (filename.IndexOf (testString,0,StringComparison.CurrentCultureIgnoreCase)!=-1)
                     {
@@ -52,8 +59,6 @@ namespace GLUGNetConsoleDEMO
                         TotalRecords++;
                     }  
                 }
-                //}
-                //}
             }
         }
     }
